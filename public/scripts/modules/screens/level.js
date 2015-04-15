edc85d9f404925597b2171f3301c3c9667a53c5a
Thomas Hunter II <me@thomashunter.name>
2015-04-15 01:04:35 -0700

    level screen buttons

--- public/scripts/modules/screens/level.js
+++ public/scripts/modules/screens/level.js
@@ -6,14 +6,30 @@ MODULE.LevelScreen = (function() {
     var LevelScreen = function() {
         this.$screen = $('#screen-level');
         this.$level = this.$screen.find('.level');
+        this.$footer = this.$screen.find('footer');
+
+        this.$buttons = {
+            play: this.$footer.find('button.play'),
+            clear: this.$footer.find('button.clear'),
+            help: this.$footer.find('button.help'),
+            library: this.$footer.find('button.library'),
+            exit: this.$footer.find('button.exit')
+        };
+
+        this.$buttons.exit.on('click', function() {
+            app.screens.campaign.display();
+        });
+
+        this.level = null;
+        this.level_id = null;
     };
 
     LevelScreen.prototype.display = function(level_id) {
         $('#screens > .screen').hide();
 
-        var level = new MODULE.Level(level_id);
+        this.level_id = level_id;
 
-        this.$level.text(level_id);
+        this.level = new MODULE.Level(level_id);
 
         this.$screen.show();
 
