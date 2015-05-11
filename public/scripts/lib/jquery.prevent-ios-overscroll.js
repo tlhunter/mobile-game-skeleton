21246b9ebd382d2062222d3b9ce0be03089f321d
Thomas Hunter II <me@thomashunter.name>
2015-05-10 20:44:11 -0700

    attempting to fix ios overscroll BS

--- public/scripts/lib/jquery.prevent-ios-overscroll.js
+++ public/scripts/lib/jquery.prevent-ios-overscroll.js
@@ -1,26 +1,8 @@
 /**
  * Prevents iOS from overscrolling the document body.
  *
- * @url http://stackoverflow.com/a/14130056/538646
+ * @url https://github.com/pinadesign/overscroll
  */
 if (navigator.userAgent.match(/iPhone|iPad|iPod/i)) {
-	//uses document because document will be topmost level in bubbling
-	$(document).on('touchmove',function(e){
-	  e.preventDefault();
-	});
-
-	//uses body because jquery on events are called off of the element they are
-	//added to, so bubbling would not work if we used document instead.
-	$('body').on('touchstart','.scrollable',function(e) {
-	  if (e.currentTarget.scrollTop === 0) {
-		e.currentTarget.scrollTop = 1;
-	  } else if (e.currentTarget.scrollHeight === e.currentTarget.scrollTop + e.currentTarget.offsetHeight) {
-		e.currentTarget.scrollTop -= 1;
-	  }
-	});
-
-	//prevents preventDefault from being called on document if it sees a scrollable div
-	$('body').on('touchmove','.scrollable',function(e) {
-	  e.stopPropagation();
-	});
+	(function(){var e,d,g,f,c=0,b=0,a;document.addEventListener("touchstart",function(h){clearInterval(a);g=h.target;f=h.target;while((window.getComputedStyle(g)["overflow-x"]!="auto"&&window.getComputedStyle(g)["overflow-x"]!="scroll")||g.parentNode==null){g=g.parentNode}while((window.getComputedStyle(f)["overflow-y"]!="auto"&&window.getComputedStyle(f)["overflow-y"]!="auto")||f.parentNode==null){f=f.parentNode}if(g.parentNode==null){g=null}if(f.parentNode==null){f=null}var i=h.touches[0];e=i.pageX;d=i.pageY},false);document.addEventListener("touchmove",function(h){clearInterval(a);h.preventDefault();var i=h.touches[0];g.scrollLeft=g.scrollLeft-(i.pageX-e);f.scrollTop=f.scrollTop-(i.pageY-d);c=(i.pageX-e);b=(i.pageY-d);e=i.pageX;d=i.pageY},false);document.addEventListener("touchend",function(h){clearInterval(a);a=setInterval(function(){g.scrollLeft=g.scrollLeft-c;f.scrollTop=f.scrollTop-b;c=c*0.9;b=b*0.9;if(c<1&&c>-1&&b<1&&b>-1){clearInterval(a)}},15)},false)})();
 }
