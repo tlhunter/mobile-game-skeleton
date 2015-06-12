bf4a2e724b771f28648bf94823760c3818e23e25
Thomas Hunter II <me@thomashunter.name>
2015-06-12 14:46:10 -0700

    gulp less building

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
+var less = require('gulp-less');
+var minifyCss = require('gulp-minify-css');
 
-gulp.task('default', function() {
-});
+gulp.task('default', [
+	'scripts',
+	'styles'
+]);
 
 gulp.task('scripts', function() {
-    //uglifyjs public/scripts/lib/*.js public/scripts/modules/*.js public/scripts/modules/screens/*.js public/scripts/bootstrap.js -o public/scripts/app.js
 	return gulp
 		.src([
 			'public/scripts/lib/*.js',
@@ -17,4 +20,18 @@ gulp.task('scripts', function() {
 		.pipe(uglify())
 		.pipe(concat('app.js'))
 		.pipe(gulp.dest('public/scripts'));
+});
+
+gulp.task('styles', function() {
+	return gulp
+		.src([
+			'public/styles/*.css',
+			'public/styles/*.less',
+			'public/styles/screens/*.css',
+			'public/styles/screens/*.less'
+		])
+		.pipe(less())
+		.pipe(concat('app.css'))
+		.pipe(minifyCss())
+		.pipe(gulp.dest('public/styles'));
 });
