c02882a1faabf594b27dd48255b06f8a7a1bc109
Thomas Hunter II <me@thomashunter.name>
2015-06-12 14:58:02 -0700

    compiled code goes to dist/, added gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,21 @@ var concat = require('gulp-concat');
 var less = require('gulp-less');
 var minifyCss = require('gulp-minify-css');
 
+var paths = {
+	scripts: [
+		'public/scripts/lib/*.js',
+	    'public/scripts/modules/*.js',
+	    'public/scripts/modules/screens/*.js',
+	    'public/scripts/bootstrap.js'
+	],
+	styles: [
+		'public/styles/*.css',
+		'public/styles/*.less',
+		'public/styles/screens/*.css',
+		'public/styles/screens/*.less'
+	]
+};
+
 gulp.task('default', [
 	'scripts',
 	'styles'
@@ -11,27 +26,26 @@ gulp.task('default', [
 
 gulp.task('scripts', function() {
 	return gulp
-		.src([
-			'public/scripts/lib/*.js',
-			'public/scripts/modules/*.js',
-			'public/scripts/modules/screens/*.js',
-			'public/scripts/bootstrap.js'
-		])
+		.src(paths.scripts)
 		.pipe(uglify())
 		.pipe(concat('app.js'))
-		.pipe(gulp.dest('public/scripts'));
+		.pipe(gulp.dest('public/dist'));
 });
 
 gulp.task('styles', function() {
 	return gulp
-		.src([
-			'public/styles/*.css',
-			'public/styles/*.less',
-			'public/styles/screens/*.css',
-			'public/styles/screens/*.less'
-		])
+		.src(paths.styles)
 		.pipe(less())
 		.pipe(concat('app.css'))
 		.pipe(minifyCss())
-		.pipe(gulp.dest('public/styles'));
+		.pipe(gulp.dest('public/dist'));
+});
+
+gulp.task('watch', function() {
+	gulp.watch(paths.scripts, [
+		'scripts'
+	]);
+	gulp.watch(paths.styles, [
+		'styles'
+	]);
 });
