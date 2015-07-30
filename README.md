2d24187bcf434dbf0a048aac3b61b506a7d297c1
Thomas Hunter II <me@thomashunter.name>
2015-07-29 22:54:50 -0700

    Cordova WIP

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# Mobile Game Skeleton
+# Strategic Game of Life
+
+## Commands
+
+    cordova build <platform>
+
+Builds the specified platform, namely `ios`, `android`, and `firefoxos`.
+
+TODO: Handle this entirely using gulp
+
+    gulp <type>
+
+Compiles one of the types `scripts`, `styles`, `html`, or if omitted, all of them.
+
+    gulp watch
+
+Compiles all three types, then when any change occurs, recompiles.
+
+    gulp data
+
+Download data from the CMS to tmp/data.json. Needed for cordova build steps.
+
+TODO: Where is this data downloaded from?
+
+## iOS Notes
+
+### Fixing the Status Bar
+
+Patch the following file:
+
+```
+platforms/ios/Game of Life/Classes/MainViewController.m
+```
+
+Add the following inside of `MainViewController`:
+
+```objc
+//fix not hide status on ios7
+- (BOOL)prefersStatusBarHidden
+{
+    return YES;
+}
+```
--- gulpfile.js
+++ gulpfile.js
@@ -1,61 +1,132 @@
+'use strict';
+
+// System
+var exec = require('child_process').exec;
+var fs = require('fs');
+
+// Local Gulp
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var less = require('gulp-less');
 var minifyCss = require('gulp-minify-css');
-var exec = require('child_process').exec;
 
-var paths = {
+// Local
+var Handlebars = require('handlebars');
+
+// Constants
+var DIST = 'www/dist';
+var CSS_FILENAME = 'app.css';
+var JS_FILENAME = 'app.js';
+
+var HTML_OUTPUT = 'www/index.html';
+
+// TODO: There's some way to clean this up with /**/...
+var PATHS = {
   scripts: [
-    'public/scripts/lib/*.js',
-    'public/scripts/modules/*.js',
-    'public/scripts/modules/screens/*.js',
-    'public/scripts/bootstrap.js'
+    'src/scripts/lib/*.js',
+    'src/scripts/modules/*.js',
+    'src/scripts/modules/screens/*.js',
+    'src/scripts/bootstrap.js'
   ],
   styles: [
-    'public/styles/*.css',
-    'public/styles/*.less',
-    'public/styles/screens/*.css',
-    'public/styles/screens/*.less'
+    'src/styles/*.less',
+    'src/styles/screens/*.less'
+  ],
+  html: [
+    'src/html/*.html',
+    'src/html/screens/*.html'
   ]
 };
 
 gulp.task('default', [
   'scripts',
-  'styles'
+  'styles',
+  'html'
 ]);
 
+// TODO: Generate Sourcemaps
 gulp.task('scripts', function() {
   return gulp
-    .src(paths.scripts)
+    .src(PATHS.scripts)
     .pipe(uglify())
-    .pipe(concat('app.js'))
-    .pipe(gulp.dest('public/dist'));
+    .pipe(concat(JS_FILENAME))
+    .pipe(gulp.dest(DIST));
 });
 
 // TODO: https://github.com/postcss/autoprefixer
+// TODO: Generate Sourcemaps
 gulp.task('styles', function() {
   return gulp
-    .src(paths.styles)
+    .src(PATHS.styles)
     .pipe(less())
-    .pipe(concat('app.css'))
+    .pipe(concat(CSS_FILENAME))
     .pipe(minifyCss())
-    .pipe(gulp.dest('public/dist'));
+    .pipe(gulp.dest(DIST));
 });
 
-gulp.task('watch', ['scripts', 'styles'], function() {
-  gulp.watch(paths.scripts, [
+// TODO: ASYNC
+gulp.task('html', function() {
+  var screens = fs.readdirSync('src/html/screens/');
+  var index = fs.readFileSync('src/html/index.html').toString();
+  var template = Handlebars.compile(index);
+
+  var data = {
+      // TODO: Read this from config.xml
+      title: "Game of Life",
+      cordova: true, // TODO: Make this dynamic
+      screens: []
+  };
+
+  for (var i = 0; i < screens.length; i++) {
+      var screen = screens[i];
+      if (screen.charAt(0) === '.') {
+        continue;
+      }
+      var content = fs.readFileSync('src/html/screens/' + screen);
+      data.screens.push(content);
+  }
+
+  fs.writeFileSync(HTML_OUTPUT, template(data));
+});
+
+gulp.task('watch', ['scripts', 'styles', 'html'], function() {
+  gulp.watch(PATHS.scripts, [
     'scripts'
   ]);
-  gulp.watch(paths.styles, [
+
+  gulp.watch(PATHS.styles, [
     'styles'
+  ]);
+
+  gulp.watch(PATHS.html, [
+    'html'
   ]);
 });
 
-gulp.task('build', function(done) {
+// TODO: Download data to tmp/data.json
+gulp.task('data', function() {
+  return console.error("need to configure gulp data command");
+});
+
+// TODO
+gulp.task('build', ['scripts', 'styles', 'html', 'data'], function(done) {
   console.log('start');
-  exec('cordova build', function() {
+  // src/audio to www
+  // src/fonts to www
+  // src/images to www
+  // tmp/data.json to www/dist/data.json
+  exec('cordova build', function() { // TODO: Divvy tasks for each platform
     console.log('fin');
     done();
   });
+});
+
+gulp.task('build-android', function() {
+});
+
+gulp.task('build-ios', function() {
+});
+
+gulp.task('build-web', function() {
 });
