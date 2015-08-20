c3eddebf5e92755040ab44ef842ed56738c3e7b2
Thomas Hunter II <me@thomashunter.name>
2015-08-19 18:45:19 -0700

    signing android removes old APK, otherwise it fails

--- bin/sign-android.sh
+++ bin/sign-android.sh
@@ -2,4 +2,5 @@
 
 # keytool -genkey -v -keystore thomashunter-mobile.keystore -alias thomashuntermobile -keyalg RSA -keysize 2048 -validity 10000
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore thomashunter-mobile.keystore ./platforms/android/build/outputs/apk/android-release-unsigned.apk thomashuntermobile
+rm ./platforms/android/build/outputs/apk/strategic-game-of-life-signed.apk
 zipalign -v 4 ./platforms/android/build/outputs/apk/android-release-unsigned.apk ./platforms/android/build/outputs/apk/strategic-game-of-life-signed.apk
